dcbfa44251f7fac9e34deedb005a1133f65615bb
Jek-San <88803418+Jek-San@users.noreply.github.com>
2023-08-31 18:16:06 +0700

    fixing  LazyLoadImg

--- src/components/lazyLoadImg/LazyLoadImage.jsx
+++ src/components/lazyLoadImg/LazyLoadImage.jsx
@@ -1,5 +1,5 @@
 import "./lazyLoadImage.css"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useInView } from "react-intersection-observer"
 
 function LazyLoadImage({ src, alt, className }) {
@@ -8,18 +8,22 @@ function LazyLoadImage({ src, alt, className }) {
     threshold: 0.1, // Adjust the threshold as needed
   })
 
+  useEffect(() => {
+    setIsLoading(true) // Reset loading state when src changes
+  }, [src])
+
   const handleImageLoad = () => {
     setIsLoading(false)
   }
 
   return (
-    <div className={` ${isLoading ? "lazy-load-container" : ""}`}>
+    <div className={` ${isLoading ? `${className} lazy-load-container` : ""}`}>
       {isLoading && <div className="loading-spinner"></div>}
       <img
         ref={ref}
         src={src}
         alt={alt}
-        className={className} // Apply the provided className
+        className={className}
         loading={inView ? "eager" : "lazy"}
         onLoad={handleImageLoad}
       />
